ff7940fac9a3ba43ce4639a6eb7841a3c7b71cf4
Ross Freeman <rfreeman1@me.com>
2017-01-22 13:55:59 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './App.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <div className='App-title'>Music Master</div>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type='text'
+              placeholder="Search for an Artist"
+            />
+            <InputGroup.Addon>
+              <Glyphicon glyph='search'></Glyphicon>
+            </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
+        <div className='Profile'>
+          <div>Artist Picture</div>
+          <div>Artist Name</div>
+        </div>
+        <div className='Gallery'>
+          Gallery
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
